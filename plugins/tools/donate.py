f698d113e4bf5a679b8980cf043e60cc59f005f4
Bikash Halder <115603304+BikashHalderNew@users.noreply.github.com>
2023-03-17 12:35:49 +0530

    Add files via upload

---
+++ plugins/tools/donate.py
@@ -0,0 +1,25 @@
+from pyrogram import Client, filters
+
+from modules import app
+
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+@app.on_message(
+    filters.command("donate")
+    & filters.group
+    & filters.private
+    & ~filters.edited & filters.group & ~filters.edited)
+async def help(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://te.legra.ph/file/f73af9a4ffe130a83d8d2.jpg",
+        caption=f"""🥀 𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞 𝐅𝐨𝐫 𝐃𝐨𝐧𝐚𝐭𝐞 🥀""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🥀 𝐎𝐰𝐧𝐞𝐫 🥀", url=f"https://t.me/BikashHalder")
+                ]
+            ]
+        ),
+    )
